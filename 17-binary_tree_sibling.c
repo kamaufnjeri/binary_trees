cb35ed1a6e792142d3fa7545b17a76bf7559cfe5
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-01 19:49:45 +0300

    perfect tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	parent = node->parent;
 	if (node == NULL || parent == NULL)
 		return (NULL);
-	if (parent->left != NULL || parent->right != NULL)
+	if (parent->left != NULL && parent->right != NULL)
 	{
 		if (node == parent->left)
 			return (parent->right);
