5b8fe76ed8c6fcc96820e2aff237ad568409fa24
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-02 15:44:23 +0300

    find ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,51 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * loop - loop through the nodes
- * @one: to loop
- * @two: to loop
- * Return: NULL or node
- */
-binary_tree_t *loop(binary_tree_t *one, binary_tree_t *two)
-{
-	binary_tree_t *node;
-
-	if (one == NULL || two == NULL)
-		return (NULL);
-
-	if (one->parent == NULL)
-	{
-		while (two->parent != NULL)
-		{
-			node = binary_trees_ancestor(one, two);
-			if (node != NULL)
-				return (node);
-		}
-	}
-	if (two->parent == NULL)
-	{
-		while (one->parent != NULL)
-		{
-			node = binary_trees_ancestor(one, two);
-			one = one->parent;
-			if (node != NULL)
-				return (node);
-		}
-	}
-	while (two->parent != NULL)
-	{
-		two = two->parent;
-		while (one->parent != NULL)
-		{
-			node = binary_trees_ancestor(one, two);
-			one = one->parent;
-			if (node != NULL)
-				return (node);
-		}
-	}
-	return (NULL);
-}
-/**
  * binary_trees_ancestor - find thte least common ancestor of two nodes
  * @first: to look
  * @second: to look
@@ -71,6 +26,18 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	two = (binary_tree_t *)second;
 	one = (binary_tree_t *)first;
 
-	node = loop(two, one);
-	return (node);
+	while (two->parent != NULL)
+	{
+		two = two->parent;
+		while (one->parent != NULL)
+		{
+			node = binary_trees_ancestor(one, two);
+			one = one->parent;
+			if (node != NULL)
+				return (node);
+			else
+				return (node);
+		}
+	}
+	return (NULL);
 }
