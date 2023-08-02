64a0790d13468cee745500b31f04885b5adad0c4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-02 15:32:37 +0300

    find ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,8 +1,17 @@
 #include "binary_trees.h"
 
+/**
+ * loop - loop through the nodes
+ * @one: to loop
+ * @two: to loop
+ * Return: NULL or node
+ */
 binary_tree_t *loop(binary_tree_t *one, binary_tree_t *two)
 {
 	binary_tree_t *node;
+
+	if (one == NULL || two == NULL)
+		return (NULL);
 
 	if (one->parent == NULL)
 	{
@@ -23,16 +32,16 @@ binary_tree_t *loop(binary_tree_t *one, binary_tree_t *two)
 		}
 	}
 	while (two->parent != NULL)
-        {
-                two = two->parent;
-                while (one->parent != NULL)
-                {
-                        node = binary_trees_ancestor(one, two);
-                        one = one->parent;
-                        if (node != NULL)
-                                return (node);
-                }
-        }
+	{
+		two = two->parent;
+		while (one->parent != NULL)
+		{
+			node = binary_trees_ancestor(one, two);
+			one = one->parent;
+			if (node != NULL)
+				return (node);
+		}
+	}
 	return (NULL);
 }
 /**
