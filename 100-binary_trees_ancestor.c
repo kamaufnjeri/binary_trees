686eba61165334fdb8676b1830a5fab530813532
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-08-02 14:14:55 +0300

    trial ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ *
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *two, *one, *node;
+
+	if (second == NULL || first == NULL)
+		return (NULL);
+	if (second == first)
+		return ((binary_tree_t *)second);
+	if (second->parent == first->parent)
+		return ((binary_tree_t *)second->parent);
+
+	if (second == first->parent)
+		return ((binary_tree_t *)first->parent);
+
+	if (first == second->parent)
+		return ((binary_tree_t *)second->parent);
+	two = (binary_tree_t *)second;
+	one = (binary_tree_t *)first;
+
+	while (two->parent != NULL)
+	{
+		two = two->parent;
+		while (one->parent != NULL)
+		{
+			node = binary_trees_ancestor(one, two);
+			one = one->parent;
+			if (node != NULL)
+				return (node);
+		}
+	}
+	return (NULL);
+}
